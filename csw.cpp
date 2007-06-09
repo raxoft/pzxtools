94c01dd62cfa2028d1412cea37d22575ee3566b1
Patrik Rak <patrik@raxoft.cz>
2007-06-09 22:11:46 +0000

    Implemented CSW decoding.

--- csw.cpp
+++ csw.cpp
@@ -5,3 +5,59 @@
 #include "csw.h"
 #include "pzx.h"
 
+/**
+ * Render CSW encoded pulses to the output stream.
+ */
+void csw_render_block( bool & level, const uint sample_rate, const uint pulse_count, const byte * const data, const uint size )
+{
+    hope( sample_rate > 0 ) ;
+    hope( data || pulse_count == 0 ) ;
+
+    const byte * p = data ;
+    const byte * const end = data + size ;
+
+    // Iterate over all pulses.
+
+    for ( uint i = 0 ; i < pulse_count ; i++ ) {
+
+        // Make sure there are still some data.
+
+        if ( p >= end ) {
+            warn( "premature end of CSW data" ) ;
+            return ;
+        }
+
+        // Fetch the pulse duration encoded as number of samples.
+
+        uint sample_count = *p++ ;
+
+        // In case it is 0, fetch the 32 bit sample count.
+
+        if ( sample_count == 0 ) {
+
+            if ( end - p < 4 ) {
+                warn( "premature end of CSW data" ) ;
+                return ;
+            }
+
+            sample_count = *p++ ;
+            sample_count += *p++ << 8 ;
+            sample_count += *p++ << 16 ;
+            sample_count += *p++ << 24 ;
+        }
+
+        // Now output the pulse, converting the sample count to duration in 3.5MHz T cycles first.
+
+        long long unsigned duration = ( ( 3500000ull * duration ) / sample_rate ) ;
+        const uint limit = 0xFFFFFFFF ;
+
+        while ( duration > limit ) {
+            pzx_out( limit, level ) ;
+            duration -= limit ;
+        }
+
+        pzx_out( uint( duration ), level ) ;
+
+        level = ! level ;
+    }
+}
--- csw.h
+++ csw.h
@@ -5,13 +5,22 @@
 #ifndef CSW_H
 #define CSW_H 1
 
-#ifndef TYPES_H
-#include "types.h"
+#ifndef BUFFER_H
+#include "buffer.h"
 #endif
 
-// CSW version we support.
+// Interface.
 
-const byte CSW_MAJOR = 2 ;
-const byte CSW_MINOR = 0 ;
+void csw_render_block(
+    bool & level,
+    const uint sample_rate,
+    const uint pulse_count,
+    const byte * const data,
+    const uint size
+) ;
+
+void csw_unpack_block( Buffer & buffer, const byte * const data, const uint size ) ;
+
+void csw_render( const byte * const data, const uint size ) ;
 
 #endif // CSW_H

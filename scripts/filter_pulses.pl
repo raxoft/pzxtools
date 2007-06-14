fea4e2fc75c44b5dc7edd0c5d5bbaba325e11e25
Patrik Rak <patrik@raxoft.cz>
2007-06-14 14:16:13 +0000

    Implemented new script.

---
+++ scripts/filter_pulses.pl
@@ -0,0 +1,35 @@
+#! /usr/bin/perl -wT
+#
+# Map pulse durations in given range to specific value.
+
+my @filters ;
+
+loop: while(<>) {
+
+    if ( my( $duration, $rest ) = /^PULSE\s+(\d+)(.*)$/i ) {
+        my @f = @filters ;
+        while ( my( $result, $min, $max ) = splice( @f, 0, 3 ) ) {
+            if ( $min <= $duration && $duration <= $max ) {
+                print "PULSE $result$rest\n" ;
+                next loop ;
+            }
+        }
+    }
+    
+    if ( my( $duration, $a, $b ) = /^FILTER\s+(\d+)\s+(\d+)\s*(\d*)/i ) {
+        if ( $b eq "" ) {
+            push( @filters, $duration, $duration - $a, $duration + $a ) ;
+        }
+        else {
+            push( @filters, $duration, $a, $b ) ;
+        }
+        next ;
+    }
+
+    if ( my( $duration, $a, $b ) = /^FILTER\s*$/i ) {
+        @filters = () ;
+        next ;
+    }
+
+    print ;
+}
